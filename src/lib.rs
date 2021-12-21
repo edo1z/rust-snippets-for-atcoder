a53c9e138dd1ec798c5d36f7769c4e43bb701a77
web3ten0 <web3ten0@gmail.com>
2021-12-21 21:44:31 +0900

    added factorial

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod hashset;
 pub mod iter;
+pub mod math;
 pub mod string;
 pub mod utils;
---
+++ src/math.rs
@@ -0,0 +1,24 @@
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+use cargo_snippet::snippet;
+use itertools::Itertools;
+
+// 階乗
+#[snippet("factorial - 階乗")]
+fn factorial(n: usize) -> usize {
+    let mut memo: Vec<usize> = vec![1; n + 1];
+    _factorial(n, &mut memo)
+}
+fn _factorial(n: usize, memo: &mut Vec<usize>) -> usize {
+    if n < 2 || memo[n] > 1 {
+        memo[n]
+    } else {
+        memo[n] = _factorial(n - 1, memo) * n;
+        memo[n]
+    }
+}
+
+#[test]
+fn test_factorial() {
+    assert_eq!(factorial(5), 120);
+    assert_eq!(factorial(6), 720);
+}
