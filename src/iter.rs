48414f46a448c7c462e4dc1cf9efc3953d726bb4
web3ten0 <web3ten0@gmail.com>
2021-12-21 12:15:26 +0900

    fix find_index

--- src/iter.rs
+++ src/iter.rs
@@ -3,7 +3,7 @@ use cargo_snippet::snippet;
 use itertools::Itertools;
 
 #[snippet("find_index")]
-fn find_index(vec: &Vec<char>, search_target: char) -> usize {
+fn find_index<T: PartialEq + Copy>(vec: &Vec<T>, search_target: T) -> usize {
     vec.iter().position(|&x| x == search_target).unwrap()
 }
 
@@ -21,4 +21,8 @@ fn test_find_index() {
     let target = 'x';
     let result = find_index(&vec, target);
     assert_eq!(result, 23);
+
+    let vec_num = vec![1,2,3,4,5];
+    let result_num = find_index(&vec_num, 3);
+    assert_eq!(result_num, 2);
 }
