81df6a7bef3776eae71204d035810d6bcdc63906
web3ten0 <web3ten0@gmail.com>
2021-12-24 22:09:02 +0900

    added fibo

--- src/math.rs
+++ src/math.rs
@@ -25,6 +25,32 @@ fn test_factorial() {
     assert_eq!(factorial(6), 720);
 }
 
+// フィボナッチ数列
+#[snippet("fibo-フィボナッチ数列")]
+#[snippet(include = "_fibo")]
+fn fibo(n: usize) -> usize {
+    let mut memo = vec![0; n + 1];
+    _fibo(n, &mut memo)
+}
+#[snippet("_fibo")]
+fn _fibo(n: usize, memo: &mut Vec<usize>) -> usize {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else if memo[n] > 0 {
+        memo[n]
+    } else {
+        memo[n] = _fibo(n - 1, memo) + _fibo(n - 2, memo);
+        memo[n]
+    }
+}
+#[test]
+fn test_fibo() {
+    assert_eq!(fibo(10), 55);
+    assert_eq!(fibo(11), 89);
+}
+
 //進数変換
 #[snippet("進数変換")]
 fn convert_digits(num: i64, digit: usize) -> String {
@@ -56,6 +82,7 @@ fn test_from_str_radix() {
     assert_eq!(from_str_radix(&String::from("400"), 8), 256);
 }
 
+// ユークリッドの互除法
 #[snippet("GCD-ユークリッドの互除法")]
 fn gcd(m: usize, n: usize) -> usize {
     if n == 0 {
